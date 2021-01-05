583a29c42deaebc6d220e5e7c7010807060a9637
brabii <collectorsgarbages@gmail.com>
2021-01-05 21:29:57 +0100

    creér jpa entity beer. closes #4

---
+++ src/main/java/com/beer/service/web/domain/Beer.java
@@ -0,0 +1,53 @@
+package com.beer.service.web.domain;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+	private UUID id;
+
+	@Version
+	private Long version;
+
+	@CreationTimestamp
+	private Timestamp createdDate;
+
+	@UpdateTimestamp
+	private Timestamp lastModifiedDate;
+
+	private String beerName;
+	private String beerStyle;
+	@Column(unique = true)
+	private String upc;
+	
+	private BigDecimal price;
+	private Integer minOnHand;
+	private Integer quantityToBrew;
+}
