9244c87e6c09999386b7df47f55ec227547214f2
brabii <collectorsgarbages@gmail.com>
2021-01-05 22:13:54 +0100

    ajouter une classe pour charger les objets beer. closes #6

--- src/main/java/com/beer/service/web/domain/Beer.java
+++ src/main/java/com/beer/service/web/domain/Beer.java
@@ -45,7 +45,7 @@ public class Beer {
 	private String beerName;
 	private String beerStyle;
 	@Column(unique = true)
-	private String upc;
+	private Long upc;
 	
 	private BigDecimal price;
 	private Integer minOnHand;
--- src/main/java/com/beer/service/web/repository/BeerRepository.java
+++ src/main/java/com/beer/service/web/repository/BeerRepository.java
@@ -1,8 +1,10 @@
-package com.beer.service.web.domain;
+package com.beer.service.web.repository;
 
 import java.util.UUID;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.beer.service.web.domain.Beer;
 
 public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
 
